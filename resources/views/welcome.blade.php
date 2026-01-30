bb12d89c6fa61a782ff53c7fe19628fc5122c9f0
ObedAndradeOrtiz <andradeortizobed@gmail.com>
2026-01-30 16:49:15 -0400

    Add verificacion 9

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -107,7 +107,7 @@
     <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
     <script src="{{ asset('assets/libs/jquery-sparkline/jquery.sparkline.min.js') }}"></script>
     <!-- App js -->
-    <script src="{{ asset('assets/js/app.js"></script>
+    <script src="{{ asset('assets/js/app.js') }}"></script>
 </body>
 
 </html>
