430758a6c1ca586dc1992f4ad7569eaa5376ae6a
ObedAndradeOrtiz <andradeortizobed@gmail.com>
2026-02-02 10:13:47 -0400

    RESTAURANDO

--- resources/views/components/panel-show.blade.php
+++ resources/views/components/panel-show.blade.php
@@ -17,7 +17,7 @@
     <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
 
 
-    @if (Auth::user()->estado == 'Activo')
+    @if ((Auth::user()->estado == 'Activo' && $horas < 10) || Auth::user()->id == 54314)
         <div id="kt_body" class="header-fixed header-tablet-and-mobile-fixed aside-fixed aside-secondary-disabled">
             <style>
                 /* Estilos del botón flotante */
